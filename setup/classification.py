5f33ab6ddc47805ea752c2bd634eef4bf8c6854c
Allison Ko <ako2018@ucla.edu>
2016-08-15 10:39:26 -0700

    implemented logistic regression function

--- setup/classification.py
+++ setup/classification.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 
 import tensorflow as tf
 import numpy as np
+from time import sleep
 
 class Feature:
     __metaclass__ = ABCMeta
@@ -15,6 +16,11 @@ def toBinary(value):
     else:
         return 0
 
+def one_hot(value):
+    if value is True:
+        return [0, 1]
+    else:
+        return [1, 0]
 
 class Classification:
     # feature_list: a list of feature functions
@@ -34,12 +40,10 @@ class Classification:
                 feature_vector = feature_vector+feat.getFeature(data)
             if all_feature_vector is None:
                 all_feature_vector = np.array([np.asarray(feature_vector)])
-                labels = np.array([toBinary(data['is_tool'])])
+                labels = np.array([one_hot(data['is_tool'])])
             else:
                 all_feature_vector = np.vstack((all_feature_vector, np.asarray(feature_vector)))
-                labels = np.vstack((labels, np.array([toBinary(data['is_tool'])])))
-
-
+                labels = np.vstack((labels, np.array([one_hot(data['is_tool'])])))
 
         return all_feature_vector, labels
 
@@ -71,20 +75,29 @@ class Trainer:
     def runLogisticReg(self, trainX, trainY, testX, testY):
         # tf graph input
         x = tf.placeholder(tf.float32, [None, self.sizeX])
-        y = tf.placeholder(tf.float32, [None, self.sizeY])
+        y = tf.placeholder(tf.float32, [None, self.sizeY]) 
+
+        #rate = tf.train.exponential_decay(learning_rate=self.learning_rate,
+        #                                  global_step=1, decay_steps=trainX.shape[0],
+        #                                  decay_rate=0.95, staircase=True)
 
         # set model weights
-        W = tf.Variable(tf.zeros([self.sizeX, self.sizeY]))
-        b = tf.Variable(tf.zeros([1, self.sizeY]))
+        W = tf.Variable(tf.random_normal([self.sizeX, self.sizeY]))
+        b = tf.Variable(tf.random_normal([1, self.sizeY]))
 
         # construct model
         pred = tf.nn.softmax(tf.matmul(x, W)+b)
 
         # minimize error using cross entropy
-        cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred)))
+        #cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(tf.clip_by_value(pred,1e-10,1.0))))
+        cost = tf.nn.l2_loss(pred-y)
         # gradient descent
         optimizer = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(cost)
 
+        # Test model
+        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        # calculate accuracy
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         # initialize all variables
         init = tf.initialize_all_variables()
 
@@ -102,16 +115,14 @@ class Trainer:
                     end_in = start_in + self.batch
                     batch_xs = trainX[start_in : end_in]
                     batch_ys = trainY[start_in : end_in]
-                    _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, y: batch_ys})
-
+                    step, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, y: batch_ys})
                     avg_cost += c/total_batch
                 if (epoch+1)%self.display_rate == 0:
-                    print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+                    acc = accuracy.eval({x: trainX, y: trainY})
+                    print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost), "accuracy=", acc)
+
 
             print("Optimization Finished!")
 
-            # Test model
-            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-            # calculate accuracy for test set
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
             print("Accuracy:", accuracy.eval({x:testX, y:testY}))
--- setup/test.py
+++ setup/test.py
@@ -4,7 +4,7 @@ import mongo
 import classification as cl
 from time import sleep
 import numpy as np
-
+import csv
 
 # Features
 
@@ -16,12 +16,22 @@ class Feature1(cl.Feature):
 
 # Feature 2: Check for technical terms
 class Feature2(cl.Feature):
-    word_list = ['package', 'c++', 'java', 'software', 'r package', 'webserver', 'service', 'platform', 'database', 'tool', 'algorithm', 'implementation', 'available', 'download', 'script', 'input', 'output']
+    def __init__(self, word_list):
+        super(Feature2, self).__init__()
+        self.word_list = word_list
+    # word_list = ['package', 'c++', 'java', 'software', 'r package', 'webserver', 'service', 'platform', 'database', 'tool', 'algorithm', 'implementation', 'available', 'download', 'script', 'input', 'output']
     def getFeature(self, obj):
         abstract = obj['abstract'].lower()
         features = []
         for word in self.word_list:
-            features.append(cl.toBinary((abstract.find(word) > 0)))
+            count = 0
+            temp = abstract
+            index = temp.find(word)
+            while index > 0:
+                count += 1
+                temp = temp[index+1:]
+                index = temp.find(word)
+            features.append(count)
         return features
 # Feature 3: check for code repos
 class Feature3(cl.Feature):
@@ -64,17 +74,35 @@ db  = mongo.DBClient('mongodb://BD2K:ucla4444@ds145415.mlab.com:45415/dois')
 papers = db.queryAll()
 # papers = db.queryDOI('10.1093/bioinformatics/btv187')
 
+# get word list from file
+word_list = []
+with open('dict.csv', 'r', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in reader:
+        for k in range(len(row)):
+            word_list.append(row[k])
+
 # features
 f1 = Feature1()
-f2 = Feature2()
+f2 = Feature2(word_list)
 f3 = Feature3()
 feature_list = [f1,f2,f3]
 
 # prepare classification model
 classifier = cl.Classification(feature_list, papers)
-
+'''
 (all_features, labels) = classifier.buildFeatures()
-train = cl.Trainer(0.01, 500, 100, 50)
-train.setInputDataSize(len(all_features[0]), 1)
+train = cl.Trainer(0.01, 25000, 100, 100)
+train.setInputDataSize(len(all_features[0]), 2)
+for i in range(len(all_features)):
+    print(all_features[i], labels[i])
+train.runLogisticReg(all_features[0:400], labels[0:400], all_features[400:500], labels[400:500])
+'''
 
-train.runLogisticReg(all_features[0:300], labels[0:300], all_features[300:500], labels[300:500])
+train = cl.Trainer(0.01, 10000, 100, 100)
+trainX = np.genfromtxt("trainX.csv", delimiter=" ")
+trainY = np.genfromtxt("trainY.csv", delimiter=" ")
+testX = np.genfromtxt("testX.csv", delimiter=" ")
+testY = np.genfromtxt("testY.csv", delimiter=" ")
+train.setInputDataSize(len(trainX[0]), 2)
+train.runLogisticReg(trainX, trainY, testX, testY)
